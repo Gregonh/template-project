ba48d83c8d5a3cb92969df3432bd9669e9fe87ff
grego_nh <gregonh@gmail.com>
2024-06-24 11:46:50 +0200

    Add some frameworks and change vite config Add react router, swr, zustand. Change the vite config to use dist as build directory.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Ensure this matches the path used in your Express server
+    outDir: "dist",
+  },
+});
