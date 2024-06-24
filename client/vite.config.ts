d813e749f799fad4a81f22ce60214677ca3cec4e
grego_nh <gregonh@gmail.com>
2024-06-24 21:38:01 +0200

    Modify the outDir of tsconfig files Change the vite ts config file, client and server ts config files to use a root dist folder for the build and compilation.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     // Ensure this matches the path used in your Express server
-    outDir: "dist",
+    outDir: '../dist/client',
   },
 });
