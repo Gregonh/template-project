e2d22c2b9ef1b34518afc04a381f42092ee57925
grego_nh <gregonh@gmail.com>
2024-06-24 19:28:31 +0200

    Add eslint Start with eslint configuration using the new flat configuration. Install the package to migrate from eslintrc.cjs to the new eslint.config.mjs. The migration is not easy.

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslint.config.mjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
